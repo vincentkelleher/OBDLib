166366984cda7c01093949b16b694aafca8f7c98
Vincent Kelleher <Vincent@MacBook-Pro-de-Vincent.local>
2014-12-23 20:18:33 +0100

    Corrected test imports

--- odblib/test/test_ODBRequest.py
+++ odblib/test/test_ODBRequest.py
@@ -1,6 +1,6 @@
 import unittest
-from ODBUtils import ODBRequest
-from ODBUtilsExceptions import InvalidResponseModeException, InvalidResponsePIDException, NoResponseException
+from ..ODBUtils import ODBRequest
+from ..ODBUtilsExceptions import InvalidResponseModeException, InvalidResponsePIDException, NoResponseException
 from mock import MagicMock
 
 
