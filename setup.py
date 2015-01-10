3624f2aabf6cb6fb8e7f4388f68a505352d01d20
Vincent Kelleher <k2r79@hotmail.fr>
2015-01-10 15:38:20 +0100

    Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='OBDLib',
+    version='',
+    packages=['obdlib', 'obdlib.test'],
+    url='https://github.com/k2r79/OBDLib',
+    license='MIT Licence',
+    author='Vincent Kelleher',
+    author_email='k2r79@hotmail.fr',
+    description='ODB II library that allows to connect and interact with a Bluetooth ELM327'
+)
